6b55445a50a1c83d9d0421e554b0cc91f9b4eebe
Ryan Altimari <ryanaltimari8@gmail.com>
2020-08-31 14:48:13 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,6 +67,8 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season, holiday|
     puts "#{season.to_s.capitalize}"
+    holiday.each do |event, supplies|
+      puts "#{event.to_s}"
       
 end
 
